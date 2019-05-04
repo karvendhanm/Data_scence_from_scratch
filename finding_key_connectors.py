fa1347e4116cdbd67634dc2dd277a5e96de68c89
Karvendhan <karvsmech@gmail.com>
2019-05-04 12:02:14 +0530

    Feature Addition: Nested list comprehension added

--- finding_key_connectors.py
+++ finding_key_connectors.py
@@ -1,71 +1,57 @@
-from __future__ import division
 import operator
-import pandas as pd
-import numpy as np
-
 
 # all available users
 users = [
-    {"id":0, "name":"Hero"},
-    {"id":1, "name":"Dunn"},
-    {"id":2, "name":"Sue"},
-    {"id":3, "name":"Chi"},
-    {"id":4, "name":"Thor"},
-    {"id":5, "name":"Clive"},
-    {"id":6, "name":"Hicks"},
-    {"id":7, "name":"Devin"},
-    {"id":8, "name":"Kate"},
-    {"id":9, "name":"Klein"}
+    {"id": 0, "name": "Hero"},
+    {"id": 1, "name": "Dunn"},
+    {"id": 2, "name": "Sue"},
+    {"id": 3, "name": "Chi"},
+    {"id": 4, "name": "Thor"},
+    {"id": 5, "name": "Clive"},
+    {"id": 6, "name": "Hicks"},
+    {"id": 7, "name": "Devin"},
+    {"id": 8, "name": "Kate"},
+    {"id": 9, "name": "Klein"}
 ]
 
 # friendship network
-friendships = [(0,1),(0,2),(1,2),(1,3),(2,3),(3,4),
-               (4,5),(5,6),(5,7),(6,8),(7,8),(8,9)]
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+# let us add another feature called "freinds" to every user
 for user in users:
     user["friends"] = []
 
-for i,j in friendships:
-    users[i]["friends"].append(users[j]) # add j as a friend of i
-    users[j]["friends"].append(users[i]) # add i as a friend of j
+for i, j in friendships:
+    users[i]["friends"].append(users[j])  # All j is added as i friend
+    users[j]["friends"].append(users[i])  # All i is added as j friend
 
-def number_of_friends(user):
+# Goal: to find the average number of freinds for each user
+## function to provide number of friends if a user is given to it
+def num_of_friends(user):
     '''
-    how many friends does an user have.
+    this function takes in an user an returns the number of friends that particular user has
+    :param user: Format dict with a feature called "friends".
+    :return: number of friends
     '''
     return len(user["friends"])
 
-total_connections = sum([number_of_friends(user) for user in users])
-print(total_connections)
+## bulid a list of tuples with id and number of friends for each user.
+num_friends_by_id = [(user["id"], num_of_friends(user)) for user in users]
+
+## Now assign users in descending order deoending upon their number of friends.
+num_friends_by_id.sort(key = operator.itemgetter(1), reverse=True)
+
+# Total number of friends for all users
+total_num_of_users = sum([num_of_friends(user) for user in users])
+avg_num_of_friends = total_num_of_users / len(users)
+
+# Data Scientists You May Know
+def friends_of_friend_ids_bad(user):
+    # return the ids of friends of a friend
+    return [foaf['id']
+            for friend in user['friends']  # for each of user's friends
+            for foaf in friend['friends']]  # get each of _their_ friends
 
 
-num_users = len(users)
-avg_connections = total_connections / num_users
-
-# create a list (user_id, number_of_friends)
-num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
-
-# sort the list of tuples(num_friends_by_id) in descending order based on 2nd element
-# num_friends_by_id.sort(key=operator.itemgetter(1), reverse=True)
-# print(num_friends_by_id)
-
-# network metric degree centrality
-sorted(num_friends_by_id, key=operator.itemgetter(1), reverse=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+friends_of_friend_ids_bad(users[0])
